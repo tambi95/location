f53ad28e9efa3c5ec78b3a7f50475df0ce3c64bc
partheepan <tambi87@hotmail.com>
2019-07-12 10:41:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -26,8 +26,12 @@
 //   - Conformité W3C par ajout de <li>...</li> sur les variables
 //     $aliasContents et $projectContents si vides
 
+<<<<<<< HEAD
 
 //[modif oto] - Pour supprimer niveau localhost dans les url
+=======
+//[modif oto] - Pour supprimer niveau localhost dans les url 
+>>>>>>> parent of c8d235f... Update index.php
 $suppress_localhost = true;
 part95
 part95140
